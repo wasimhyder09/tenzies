ec40c8ee5010c48df05eea7fc2045365003c222a
Wasim Hyder <wasim@creativefaze.com>
2022-09-19 17:02:33 +0500

    Created dice styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import Die from './components/Die'
 import './App.css';
 
 function App() {
   return (
     <div className="container">
+      <div className="dice-container">
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+      </div>
     </div>
   );
 }
