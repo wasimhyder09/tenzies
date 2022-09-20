f7b5568bccf97cdf776720ac382fd822df67bb92
Wasim Hyder <wasim@creativefaze.com>
2022-09-20 23:03:15 +0500

    Created wig game workflow

--- src/App.js
+++ src/App.js
@@ -8,25 +8,63 @@ function App() {
   function allNewDice() {
     const newDice = []
     for(let i=1; i<= 10; i++) {
-      newDice.push({
-        value: (Math.floor(Math.random() * 6) + 1),
-        isHeld: false,
-        id: nanoid()
-      })
+      newDice.push(generateNewDie())
     }
     return newDice;
   }
 
-  const[dice, setDice] = useState(allNewDice())
+  function generateNewDie() {
+    return {
+      value: (Math.floor(Math.random() * 6) + 1),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
 
-  const diceElements = dice.map(die => <Die key={die.id} value={die.value} />)
+  const[dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
+
+  useEffect(() => {
+    let firstDie = dice[0].value
+    let heldCount = 0
+    dice.forEach((die) => {
+      if(die.isHeld === true && die.value === firstDie) {
+        heldCount++
+      }
+    })
+    if(heldCount == 10) {
+      setTenzies(true)
+    }
+  }, [dice])
+
+  const diceElements = dice.map(die => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ))
 
   function rollDice() {
-    setDice(allNewDice())
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld === true ? die : generateNewDie()
+    }))
   }
+
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ?
+        {...die, isHeld: !die.isHeld} :
+        die
+    }))
+  }
+
 
   return (
     <div className="container">
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
